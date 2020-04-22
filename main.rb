7803b2258b2e106a270d91e2110144f683bff6f2
Jagepard <dankorot@gmail.com>
2020-04-22 17:55:12 +0300

    delete unused require

--- main.rb
+++ main.rb
@@ -3,7 +3,6 @@ author  : Jagepard <jagepard@yandex.ru>
 license https://mit-license.org/ MIT
 =end
 
-require "./Chain"
 require "./NoticeHandler"
 require "./WarningHandler"
 require "./ErrorHandler"
