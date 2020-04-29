33022aa40130aca55de059398ba96f513587381a
Jagepard <dankorot@gmail.com>
2020-04-29 14:23:34 +0300

    reformat

--- Handler.rb
+++ Handler.rb
@@ -4,22 +4,22 @@ license https://mit-license.org/ MIT
 =end
 
 class Handler
-    def execute(request)
-        if request === self.class.name
-            puts(self.class.name + " has handle an error")
-            return
-        end
-
-        if @next === nil
-            raise "Handler does not exist in the chain"
-        end
-
-        @next.execute(request)
+  def execute(request)
+    if request === self.class.name
+      puts(self.class.name + " has handle an error")
+      return
     end
 
-    # @param [Handler] handler
-    def set_next(handler)
-        @next = handler
-        handler
+    if @next === nil
+      raise "Handler does not exist in the chain"
     end
+
+    @next.execute(request)
+  end
+
+  # @param [Handler] handler
+  def set_next(handler)
+    @next = handler
+    handler
+  end
 end
